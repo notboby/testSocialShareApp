409b160ac2f0f2d9652f063b84ca1496a420dba3
Notboby <RobinFabre@t.home>
2015-11-30 10:20:31 +0100

    iOS single view app social share text test

--- testSocialShareApp/ViewController.m
+++ testSocialShareApp/ViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "ViewController.h"
+#import "Social/Social.h"
+
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *tweetTextView;
+
+
+- (void) showAlertMessage:(NSString *) myMessage;
+
+@property (weak, nonatomic) IBOutlet UITextView *facebookTextView;
+
+@property (weak, nonatomic) IBOutlet UITextView *shareTextView;
+
 
 @end
 
@@ -16,6 +28,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +36,96 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void) showAlertMessage:(NSString *) myMessage{
+    UIAlertController *alertController;
+    alertController = [UIAlertController alertControllerWithTitle:@"TwitterShare" message:myMessage preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alertController animated:YES completion:nil];
+    
+}
+
+// Share button 1
+- (IBAction)twitterShareAction:(id)sender {
+    
+    if([self.tweetTextView isFirstResponder]){
+        [self.tweetTextView resignFirstResponder];
+    }
+    
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+        
+        SLComposeViewController *twitterVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        
+        if([self.tweetTextView.text length] < 140){
+            [twitterVC setInitialText:self.tweetTextView.text];
+            
+        }
+        else{
+            NSString *shortText = [self.tweetTextView.text substringToIndex:140];
+            [twitterVC setInitialText:shortText];
+        }
+        
+        [self presentViewController:twitterVC animated:YES completion:nil];
+        
+    }
+    else{
+        [self showAlertMessage:@"Vous n'êtes pas connecté à Twitter"];
+        
+    }
+
+
+    
+    
+    
+}
+
+// Share button 2
+
+- (IBAction)facebookShareAction:(id)sender {
+    
+    if([self.facebookTextView isFirstResponder]){
+        [self.facebookTextView resignFirstResponder];
+    }
+    
+    if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]){
+        SLComposeViewController *facebookVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        [facebookVC setInitialText:self.facebookTextView.text];
+        [self presentViewController:facebookVC animated:YES completion:nil];
+        
+    }else{
+        [self showAlertMessage:@"Veuillez vous connecter à Facebook"];
+    }
+
+
+
+
+
+}
+
+// Share button 3
+
+- (IBAction)shareAction:(id)sender {
+    if([self.shareTextView isFirstResponder]){
+        [self.shareTextView resignFirstResponder];
+    }
+    
+    UIActivityViewController *moreVC = [[UIActivityViewController alloc]initWithActivityItems:@[self.shareTextView.text] applicationActivities:nil];
+    
+    [self presentViewController:moreVC animated:YES completion:nil];
+
+
+
+}
+
+// Share button 4
+
+- (IBAction)nothingAction:(id)sender {
+    [self showAlertMessage:@"Je ne fais rien"];
+}
+
+
+
+
 
 @end
